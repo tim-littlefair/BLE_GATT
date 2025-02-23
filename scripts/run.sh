1fea446bcfceac97ae8f0dea7e36f0e21973aaae
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-02-23 11:07:16 +0800

    Documented pip hacks required to get a viable virtual environment
    As if this commit connection fails on the line:
    ret = instance._bus.con.call_sync(
    with an error message
    gi.repository.GLib.GError: g-io-error-quark: GDBus.Error:org.bluez.Error.Failed: br-connection-unknown (36)

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,17 +5,34 @@ venvdir=.venv3
 if [ "$1" = "--clean" ]
 then
     rm -rf $venvdir
+    shift
 fi
 
 if [ ! -d $venvdir ]
 then
     /usr/bin/python3 -m venv $venvdir
     . $venvdir/bin/activate
-    pip install PyGObject
+
+    # Getting a viable virtual environment was hard work, and involved looking 
+    # at the pages listed in the next three comments.
+
+    # https://github.com/vext-python/vext/issues/95 also #82, #83, #67
+    # The fix for these turned out to be that there are no released PyPi packages which 
+    # work with up to date versions of the declared dependencies of vext and/or vext.gi, 
+    # so we need to build from a specific commits on the unreleased master branches.
     pip install --no-binary=vext https://github.com/vext-python/vext/archive/32ad4d1.zip
     pip install --no-binary=vext.gi https://github.com/vext-python/vext.gi/archive/c7a8350.zip
-    #vext -i gi.vext
-    vext -i $venvdir/share/vext/specs/gi.vext
+
+    # https://stackoverflow.com/questions/79138002/no-module-named-gi-after-building-onefile-executable-with-pyinstaller
+    # Neither BLE_GATT nor vext.gi declares a dependency on PyGOBject, but it turns out that 
+    # installing it is necessary to avoid failing with "'No module named 'gi'"
+    pip install PyGObject
+
+    # https://stackoverflow.com/questions/69919970/no-module-named-distutils-util-but-distutils-is-installed
+    # At one point the first run of BLE_GATT was failing on an import referencing distutils.
+    # This was not fixed by adding a pip dependency, but by running 'sudo apt install setuptools'
+    # to add them to the OS's base python installation.
+
     pip install BLE_GATT
 else
     . $venvdir /bin/activate
